dadf2d3a7adb689ca9a0feddd59528fffca1eef6
sanenchan <santosonela98@gmail.com>
2025-09-15 13:20:27 +0700

    update

--- database/migrations/2025_09_13_024236_create_pegawais_table.php
+++ database/migrations/2025_09_13_024236_create_pegawais_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('pegawais', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id_pegawai');
             $table->string('kode_pegawai')->unique();
             $table->string('nama_pegawai');
             $table->text('alamat')->nullable();
